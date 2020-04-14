df9b96db2f8ba502b680ac8127ff004e13c47a49
Maheshbokhani <mahesh.bokhani@bacancytechnology.com>
2020-04-14 18:28:51 +0530

    set bridge in android

--- src/components/Location.js
+++ src/components/Location.js
@@ -4,7 +4,7 @@
 
 import React, { Component } from 'react';
 import {
-    View, Text
+    View, Text, NativeModules, NativeEventEmitter, TouchableOpacity
 } from 'react-native';
 // Step-2
 // import RNLocation
@@ -23,10 +23,11 @@ export default class Location extends Component {
 
     async componentDidMount() {
 
+
         // Step-3
         // Impilement configure
 
-        // await Geolocation.getCurrentPosition(
+        // Geolocation.getCurrentPosition(
         //     position => {
         //         console.warn('in geo location')
         //         this.setState({
@@ -35,32 +36,38 @@ export default class Location extends Component {
         //         })
         //         console.warn("Lati from get notice " + this.state.latitute + " " + " Longi  from get notice" + this.state.longitute)
         //     },
+        //     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
         // )
 
-        RNLocation.requestPermission({
-            ios: 'whenInUse', // or 'always'
-            android: {
-                detail: 'coarse', // or 'fine'
-                rationale: {
-                    title: "We need to access your location",
-                    message: "We use your location to show where you are on the map",
-                    buttonPositive: "OK",
-                    buttonNegative: "Cancel"
-                }
-            }
-        }).then(granted => {
-            if (granted) {
-                this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
-                    console.log('Location', locations)
-                })
-            }
-        })
+        // Geolocation.watchPosition(position => {
+        //     const lastPosition = JSON.stringify(position);
+        //     console.log('Loc', lastPosition)
+        // });
+
+        // RNLocation.requestPermission({
+        //     ios: 'whenInUse', // or 'always'
+        //     android: {
+        //         detail: 'coarse', // or 'fine'
+        //         rationale: {
+        //             title: "We need to access your location",
+        //             message: "We use your location to show where you are on the map",
+        //             buttonPositive: "OK",
+        //             buttonNegative: "Cancel"
+        //         }
+        //     }
+        // }).then(granted => {
+        //     if (granted) {
+        //         this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
+        //             console.log('Location', locations)
+        //         })
+        //     }
+        // })
 
 
-        RNLocation.getCurrentPermission()
-            .then(currentPermission => {
-                console.log('Location1', currentPermission)
-            })
+        // RNLocation.getCurrentPermission()
+        //     .then(currentPermission => {
+        //         console.log('Location1', currentPermission)
+        //     })
 
         await RNLocation.getLatestLocation({ timeout: 60000 })
             .then(latestLocation => {
@@ -71,38 +78,41 @@ export default class Location extends Component {
             })
 
 
-        RNLocation.subscribeToLocationUpdates(locations => {
-            console.log('updatelocation', locations)
-        })
+        // RNLocation.subscribeToLocationUpdates(locations => {
+        //     console.log('updatelocation', locations)
+        // })
 
     }
 
+
     render() {
 
-        // RNLocation.configure({
-        //     distanceFilter: 100, // Meters
-        //     desiredAccuracy: {
-        //         ios: "best",
-        //         android: "balancedPowerAccuracy"
-        //     },
-        //     // Android only
-        //     androidProvider: "auto",
-        //     interval: 5000, // Milliseconds
-        //     fastestInterval: 10000, // Milliseconds
-        //     maxWaitTime: 5000, // Milliseconds
-        //     // iOS Only
-        //     activityType: "other",
-        //     allowsBackgroundLocationUpdates: false,
-        //     headingFilter: 1, // Degrees
-        //     headingOrientation: "portrait",
-        //     pausesLocationUpdatesAutomatically: false,
-        //     showsBackgroundLocationIndicator: false,
-        // })
+        RNLocation.configure({
+            distanceFilter: 100, // Meters
+            desiredAccuracy: {
+                ios: "best",
+                android: "balancedPowerAccuracy"
+            },
+            // Android only
+            androidProvider: "auto",
+            interval: 5000, // Milliseconds
+            fastestInterval: 10000, // Milliseconds
+            maxWaitTime: 5000, // Milliseconds
+            // iOS Only
+            activityType: "other",
+            allowsBackgroundLocationUpdates: false,
+            headingFilter: 1, // Degrees
+            headingOrientation: "portrait",
+            pausesLocationUpdatesAutomatically: false,
+            showsBackgroundLocationIndicator: false,
+        })
 
 
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Latitute:: {this.state.latitute}</Text>
+                <TouchableOpacity onPress={() => NativeModules.LocationModule.LocationPermission()}>
+                    <Text>Latitute:: {this.state.latitute}</Text>
+                </TouchableOpacity>
                 <Text>Longitude:: {this.state.longitute}</Text>
             </View>
         )
