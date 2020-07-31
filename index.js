28503a772b39b0dd3000281acd5ab1557e5ca35a
Maheshbokhani <mahesh.bokhani@bacancytechnology.com>
2020-07-31 15:02:46 +0530

    set web in react native

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/components/Speedometer';
+import App from './src/components/SignIn';
 import { name as appName } from './app.json';
-
+console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => App);
+
